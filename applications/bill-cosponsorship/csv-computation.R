c1f08b26f2ecd517e0329c5aba18139639ca24bd
Mirko Signorelli <20061736+mirkosignorelli@users.noreply.github.com>
2021-05-10 16:07:03 +0200

    Rerun with new multiple testing correction method (Heyse's)

--- applications/bill-cosponsorship/csv-computation.R
+++ applications/bill-cosponsorship/csv-computation.R
@@ -1,62 +1,55 @@
-rm(list = ls())
 library(igraph)
-source('B-one-tailed-neat.R')
-source('C-compute-CSV.R')
+source('functions/B-one-tailed-neat.R')
+source('functions/C-compute-CSV.R')
 
-load('clustering-without-metadata.RData')
+load('results/2-results-no-metadata.RData')
 
 alpha = 0.05
 
-csv_lou = compute_indexes(communities = communities(lou), 
-                  sizes = table(lou$membership), 
+csv_lou = compute_csv(communities = communities(lou), 
                   adj = adjacency, v = 663, alpha = alpha, 
-                  correction = 'BH')
+                  mtc = 'Heyse')
 
-csv_wt = compute_indexes(communities = communities(wt), 
-                              sizes = table(wt$membership), 
+csv_wt = compute_csv(communities = communities(wt), 
                               adj = adjacency, v = 663, alpha = alpha, 
-                              correction = 'BH')
+                              mtc = 'Heyse')
 
-csv_le = compute_indexes(communities = communities(le), 
-                         sizes = table(wt$membership), 
+csv_le = compute_csv(communities = communities(le), 
                          adj = adjacency, v = 663, alpha = alpha, 
-                         correction = 'BH')
-csv_fg = compute_indexes(communities = communities(fg), 
-                         sizes = table(fg$membership), 
+                         mtc = 'Heyse')
+csv_fg = compute_csv(communities = communities(fg), 
                          adj = adjacency, v = 663, alpha = alpha, 
-                         correction = 'BH')
+                         mtc = 'Heyse')
 
-load('clustering-with-metadata.RData')
+load('results/3-results-metadata.RData')
 
 names(comm.party.3) = names(comm.gender.3) = LETTERS[1:3]
 names(comm.party.4) = names(comm.gender.4) = LETTERS[1:4]
 
-csv_newman_party_3 = compute_indexes(communities = comm.party.3, 
-                         sizes = table(newman.party.3), 
+csv_newman_party_3 = compute_csv(communities = comm.party.3, 
                          adj = adjacency, v = 663, alpha = alpha, 
-                         correction = 'BH')
+                         mtc = 'Heyse')
 
-csv_newman_gender_3 = compute_indexes(communities = comm.gender.3, 
-                                   sizes = table(newman.gender.3), 
+csv_newman_gender_3 = compute_csv(communities = comm.gender.3, 
                                    adj = adjacency, v = 663, alpha = alpha, 
-                                   correction = 'BH')
+                                   mtc = 'Heyse')
 
-csv_newman_party_4 = compute_indexes(communities = comm.party.4, 
-                                     sizes = table(newman.party.4), 
+csv_newman_party_4 = compute_csv(communities = comm.party.4, 
                                      adj = adjacency, v = 663, alpha = alpha, 
-                                     correction = 'BH')
+                                     mtc = 'Heyse')
 
-csv_newman_gender_4 = compute_indexes(communities = comm.gender.4, 
-                                   sizes = table(newman.gender.4), 
+csv_newman_gender_4 = compute_csv(communities = comm.gender.4, 
                                    adj = adjacency, v = 663, alpha = alpha, 
-                                   correction = 'BH')
+                                   mtc = 'Heyse')
 
+# CSV for partitions with 4 clusters
 csv_wt
 csv_le
 csv_lou
 csv_newman_party_4
 csv_newman_gender_4
 
+# CSV for partitions with 3 clusters
 csv_fg
 csv_newman_party_3
 csv_newman_gender_3
