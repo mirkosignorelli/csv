eeff5ce5343d469f6b9823dc7ef4d3d139957226
Mirko Signorelli <20061736+mirkosignorelli@users.noreply.github.com>
2021-05-10 16:10:42 +0200

    Rerun with new multiple testing correction method (Heyse's)

--- applications/gene-networks/code-data-analysis-1-csv-computation.R
+++ applications/gene-networks/code-data-analysis-1-csv-computation.R
@@ -1,10 +1,14 @@
+if (on.shark) load('data/adjlist-Gambardella.RData')
+if (!on.shark) load('Gambardella_v1_2018/adjlist-Gambardella.RData')
+# all adj matrices have the same size:
+sapply(adjlist, dim)
+
 library(igraph)
 library(Matrix)
-source('B-one-tailed-neat.R')
-source('C-compute-CSV.R')
-load('adjlist-Gambardella.RData')
+source('functions/B-one-tailed-neat.R')
+source('functions/C-compute-CSV.R')
 
-compute_CSV = function(adj1, adj2, mtc = c('BH', 'none')) {
+CSV_two_graphs = function(adj1, adj2, mtc = 'Heyse') {
   keep1 = which(rowSums(adj1) != 0)
   keep2 = which(rowSums(adj2) != 0)
   keepboth = intersect(keep1, keep2)
@@ -17,31 +21,31 @@ compute_CSV = function(adj1, adj2, mtc = c('BH', 'none')) {
   nc1 = max(cl1$membership)
   setlist1 = vector('list', nc1)
   for (i in 1:nc1) {
-    id = as.numeric(which(membership(cl1) == i))
-    setlist1[[i]] = keepboth[id]
+    setlist1[[i]] = which(membership(cl1) == i)
   }
   names(setlist1) = paste('community', 1:nc1)
-    nc2 = max(cl2$membership)
+  nc2 = max(cl2$membership)
   setlist2 = vector('list', nc2)
   for (i in 1:nc2) {
-    id = as.numeric(which(membership(cl2) == i))
-    setlist2[[i]] = keepboth[id]
+    setlist2[[i]] = which(membership(cl2) == i)
   }
   names(setlist2) = paste('community', 1:nc2)
-  csv_within_g1 = compute_indexes(communities = setlist1,
-                                  adj = adj1, nodes = keepboth, alpha = 0.05, 
-                                  correction = mtc)
-  csv_within_g2 = compute_indexes(communities = setlist2,
-                                  adj = adj2, nodes = keepboth, alpha = 0.05, 
-                                  correction = mtc)
-  csv_c1_g2 = compute_indexes(communities = setlist1,
-                              adj = graph2, nodes = keepboth, alpha = 0.05, 
-                              correction = mtc)
-  csv_c2_g1 = compute_indexes(communities = setlist2,
-                              adj = graph1, nodes = keepboth, alpha = 0.05, 
-                              correction = mtc)
-  out = list('csv_c1_g2' = csv_c1_g2, 'csv_c2_g1' = csv_c2_g1, 
-             'csv_within_g1' = csv_within_g1, 'csv_within_g2' = csv_within_g2)
+  csv_within_g1 = compute_csv(communities = setlist1,
+                     adj = adj1, v = length(keepboth), 
+                     alpha = 0.05, mtc = mtc)
+  csv_within_g2 = compute_csv(communities = setlist2,
+                     adj = adj2, v = length(keepboth), 
+                     alpha = 0.05, mtc = mtc)
+  csv_c1_g2 = compute_csv(communities = setlist1,
+                     adj = graph2, v = length(keepboth),
+                     alpha = 0.05, mtc = mtc)
+  csv_c2_g1 = compute_csv(communities = setlist2,
+                     adj = graph1, v = length(keepboth),
+                     alpha = 0.05, mtc = mtc)
+  out = list('csv_c1_g2' = csv_c1_g2, 
+             'csv_c2_g1' = csv_c2_g1, 
+             'csv_within_g1' = csv_within_g1, 
+             'csv_within_g2' = csv_within_g2)
   return(out)
 }
 
@@ -50,14 +54,15 @@ relative_csv = matrix(NA, 30, 30)
 
 for (i in 1:29) {
   for (j in (i+1):30) {
-    temp <- compute_CSV(adjlist[[i]], adjlist[[j]], mtc = 'BH')
+    temp <- CSV_two_graphs(adjlist[[i]], adjlist[[j]], mtc = 'BH')
     absolute_csv[i,j] = temp$csv_c1_g2$unw_index
     absolute_csv[j,i] = temp$csv_c2_g1$unw_index
     relative_csv[i,j] = temp$csv_c1_g2$unw_index/temp$csv_within_g1$unw_index
     relative_csv[j,i] = temp$csv_c2_g1$unw_index/temp$csv_within_g2$unw_index
+    cat(j)
   }
-  #print(i)
+  print(paste('i =', i, 'done'))
 }
 
-save.image(file = 'results.RData')
-
+save(relative_csv, absolute_csv,
+  file = 'results/1_CSV_values.RData')
--- applications/gene-networks/code-data-analysis-2-dendrogram.R
+++ applications/gene-networks/code-data-analysis-2-dendrogram.R
@@ -1,9 +1,65 @@
-load('results.RData')
+load('results/1_CSV_values.RData')
+load('Gambardella_v1_2018/adjlist-Gambardella.RData')
 
 rownames(absolute_csv) = names(adjlist)
 colnames(absolute_csv) = names(adjlist)
 rownames(relative_csv) = names(adjlist)
 colnames(relative_csv) = names(adjlist)
+
+names(adjlist)
+# mGland = ghiandola mammaria
+# adrenal = ghiandole adrenali, sopra il fegato
+# skm = sketal muscle
+# lymph = linfonodo
+# umbell = cordone ombellicale
+
+group1 = c('cerebellum', 'cerebrum', 'midBrain')
+group2 = c('bronchus', 'lung')
+group3 = c('colon', 'duodenum', 'intestine')
+group4 = c('ovary', 'prostate', 'testis', 'uterus')
+group5 = c('kidney', 'liver')
+
+summary(as.vector(absolute_csv), na.rm=T)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+#0.02105 0.33333 0.40000 0.40938 0.50000 0.85714      30 
+
+absolute_csv[group1, group1]
+absolute_csv[group2, group2]
+absolute_csv[group3, group3]
+absolute_csv[group4, group4]
+absolute_csv[group5, group5]
+
+summary(as.vector(relative_csv), na.rm=T)
+#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+#0.02105 0.33333 0.40000 0.40938 0.50000 0.85714      30 
+
+relative_csv[group1, group1]
+relative_csv[group2, group2]
+relative_csv[group3, group3]
+relative_csv[group4, group4]
+relative_csv[group5, group5]
+
+q = as.vector(relative_csv[!is.na(relative_csv)])
+ordered_relative_csv = q[order(q)]; rm(q)
+
+relative_csv_list = matrix(NA, 30*29, 3)
+k = 1
+for (i in 1:30) {
+  for (j in ((1:30)[-i])) {
+    relative_csv_list[k,1] = names(adjlist)[i]
+    relative_csv_list[k,2] = names(adjlist)[j]
+    relative_csv_list[k,3] = relative_csv[i,j]
+    k = k+1
+  }
+}
+
+row_ids = which(relative_csv_list[,3] %in% tail(ordered_relative_csv, 30))
+
+relative_csv_list[row_ids,]
+
+row_ids = which(relative_csv_list[,3] %in% head(ordered_relative_csv, 30))
+
+relative_csv_list[row_ids,]
 
 rownames(relative_csv)[2] = 'adrenal gland'
 rownames(relative_csv)[4] = 'bone marrow'
@@ -12,6 +68,7 @@ rownames(relative_csv)[17] = 'mammary gland'
 rownames(relative_csv)[25] = 'skeletal muscle'
 rownames(relative_csv)[26] = 'brain stem'
 rownames(relative_csv)[29] = 'umbell. cord'
+
 
 symm_relative = (relative_csv + t(relative_csv)) / 2
 diag(symm_relative) = 1
@@ -28,8 +85,8 @@ library(circlize)
 dend = as.dendrogram(hc)
 dend <- color_branches(dend, h = 0.46)
 
-pdf('dendrogram.pdf', width = 9)
-#plot(dend, horiz = T)
+pdf('figs/2_dendrogram.pdf', width = 9)
+plot(dend, horiz = T)
 circlize_dendrogram(dend, cex = 0.8, labels_track_height = 0.3)
 dev.off()
 
